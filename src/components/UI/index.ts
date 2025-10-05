3308cfb69a0364f6835a6bd3a4e0432b2dd38114
Vlad <vlad.herasymovychh@gmail.com>
2025-10-05 15:04:54 +0200

    feat: create UI component index file to export Input, Select, Table, Toast, and Chip components

---
+++ src/components/UI/index.ts
@@ -0,0 +1,7 @@
+import Input from './Input.vue'
+import Select from './Select.vue'
+import Table from './Table.vue'
+import Toast from './Toast.vue'
+import Chip from './Chip.vue'
+
+export { Input, Select, Table, Toast, Chip }
