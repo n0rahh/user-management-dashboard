f576e7cd081c49df8e5808b26609223ea1363d2f
Vlad <vlad.herasymovychh@gmail.com>
2025-10-05 14:01:37 +0200

    chore: add SCSS preprocessor options to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/colors.scss" as *;
+        `,
+      },
+    },
+  },
   server: {
     port: 3000,
   },
